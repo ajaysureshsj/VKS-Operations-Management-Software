1411f15be6be08b0797eb3dcfa0f631ff3f12524
ajaysureshsj <ajaysureshsjofcl@gmail.com>
2025-06-27 17:03:58 +0530

    implement update and delete functionality for invoices, and fix invoice creation validation

--- src/controllers/invoice.controller.js
+++ src/controllers/invoice.controller.js
@@ -36,7 +36,6 @@ const createInvoice = asyncHandler(async (req, res) => {
 
   if (
     [
-      invoiceId,
       invoiceDate,
       vehicle,
       client,
@@ -46,7 +45,7 @@ const createInvoice = asyncHandler(async (req, res) => {
       subsidyAmount,
       totalAmount,
       notes,
-    ].some((field) => field?.trim() == "")
+    ].some((field) => field?.trim == "")
   ) {
     throw new ApiError(400, "All fields are required");
   }
@@ -58,7 +57,7 @@ const createInvoice = asyncHandler(async (req, res) => {
     throw new ApiError(500, "Unable to create invoiceId");
   }
 
-  const invoice = Invoice.create({
+  const invoice = await Invoice.create({
     invoiceId,
     invoiceDate,
     vehicle,
@@ -81,4 +80,83 @@ const createInvoice = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, invoice, "Invoice created successfully"));
 });
 
-export { getInvoice, createInvoice };
+const updateInvoice = asyncHandler(async (req, res) => {
+  const { invoiceId } = req.query;
+  const {
+    invoiceDate,
+    vehicle,
+    client,
+    servicesProvided,
+    subTotal,
+    subsidyPercentage,
+    subsidyAmount,
+    totalAmount,
+    notes,
+  } = req.body;
+
+  if (!invoiceId) {
+    throw new ApiError(400, "invoiceId is required");
+  }
+  if (
+    [
+      invoiceId,
+      invoiceDate,
+      vehicle,
+      client,
+      servicesProvided,
+      subTotal,
+      subsidyPercentage,
+      subsidyAmount,
+      totalAmount,
+      notes,
+    ].some((field) => field?.trim == "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const updatedInvoice = await Invoice.findByIdAndUpdate(
+    invoiceId,
+    {
+      $set: {
+        updatedBy: req.admin._id,
+        invoiceDate,
+        vehicle,
+        client,
+        servicesProvided,
+        subTotal,
+        subsidyPercentage,
+        subsidyAmount,
+        totalAmount,
+        notes,
+      },
+    },
+    { new: true }
+  );
+
+  if (!updateInvoice) {
+    throw new ApiError(500, "Unable to update invoice");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, updatedInvoice, "Invoice updated successfully"));
+});
+
+const deleteInvoice = asyncHandler(async (req, res) => {
+  const { invoiceId } = req.query;
+  if (!invoiceId) {
+    throw new ApiError(400, "invoiceId is required");
+  }
+
+  const response = await Invoice.findByIdAndDelete(invoiceId);
+
+  if (!response) {
+    throw new ApiError(500, "Unable to delete invoice");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, response, "Invoice deleted successfully"));
+});
+
+export { getInvoice, createInvoice, updateInvoice, deleteInvoice };
--- src/routes/invoice.route.js
+++ src/routes/invoice.route.js
@@ -1,10 +1,18 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { createInvoice } from "../controllers/invoice.controller.js";
+import {
+  createInvoice,
+  deleteInvoice,
+  getInvoice,
+  updateInvoice,
+} from "../controllers/invoice.controller.js";
 
 const router = Router();
 
+router.route("/view").get(verifyJWT, upload.none(), getInvoice);
 router.route("/create").post(verifyJWT, upload.none(), createInvoice);
+router.route("/update").patch(verifyJWT, upload.none(), updateInvoice);
+router.route("/delete").delete(verifyJWT, upload.none(), deleteInvoice);
 
 export default router;
